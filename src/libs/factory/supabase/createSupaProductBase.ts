d7f471512173fac255307cbb6fb33437f4b1249b
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-11-21 15:36:44 +0700

    refactor: update availability on product base

--- src/libs/factory/supabase/createSupaProductBase.ts
+++ src/libs/factory/supabase/createSupaProductBase.ts
@@ -1,4 +1,10 @@
-export const createSupaProductBase = ({ item }: { item: any }) => {
+import { Product } from '@/libs/@types';
+
+export const createSupaProductBase = ({
+    item,
+}: {
+    item: any;
+}): Pick<Product, 'url' | 'uri' | 'slug' | 'title' | 'bannerTitle' | 'description' | 'availability'> => {
     return {
         url: item?.url,
         uri: item?.uri,
@@ -6,5 +12,6 @@ export const createSupaProductBase = ({ item }: { item: any }) => {
         title: item?.title,
         bannerTitle: item?.banner_title,
         description: item?.description,
+        availability: item?.availability,
     };
 };
