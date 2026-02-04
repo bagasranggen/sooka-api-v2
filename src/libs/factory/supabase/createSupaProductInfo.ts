7bcdf6d2ed691d5e5e561b5d02e38818d587719c
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-04 15:24:48 +0700

    refactor: get products' badge data

--- src/libs/factory/supabase/createSupaProductInfo.ts
+++ src/libs/factory/supabase/createSupaProductInfo.ts
@@ -1,6 +1,7 @@
 import { getSupaCategories } from '../../utils/supabase/getSupaCategories';
 import { getSupaMedia } from '../../utils/supabase/getSupaMedia';
 import { getSupaPrices } from '../../utils/supabase/getSupaPrices';
+import { getSupaTags } from '../../utils/supabase/getSupaTags';
 
 export const createSupaProductInfo = async ({ item }: { item: any }) => {
     const MEDIA_ASSETS_HANDLES = [
@@ -16,6 +17,7 @@ export const createSupaProductInfo = async ({ item }: { item: any }) => {
 
     return {
         prices: await getSupaPrices({ table: 'products_prices', id: item?.id }),
+        badge: await getSupaTags({ id: item?.badge_id }),
         category: await getSupaCategories({ id: item?.category_id }),
         thumbnail: await getSupaMedia({
             table: 'media_product',
